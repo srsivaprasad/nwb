1b02ed85af810e5738bfda4637ba3e52d55ae385
Sivaprasad S R <sr.sivaprasad@gmail.com>
2017-09-21 17:05:11 +0530

    Init

---
+++ Resources/Blog.js
@@ -0,0 +1,67 @@
+﻿[
+                        {
+                            "Title": "Transform your Android App to Material Design",
+                            "Description": "Nowadays Google’s Material Design is getting popular in web application as well as in mobile application. So, in this blog, let me try and explain to you briefly how to give Google’s Material Design to Android applications.....",
+                            "Link": "http://blog.logiticks.com/transform-your-android-app-to-material-design/"
+                        },
+                        {
+                            "Title": "Signing the APK for Distribution / Publishing in play store",
+                            "Description": "After making a creative application, we need to distribute the application to the public. The best way to distribute the Android apps is through Google Play Store. Other channels are also available such as e-mail, a private web server.....",
+                            "Link": "http://blog.logiticks.com/signing-the-apk-for-distribution-ed/"
+                        },
+                        {
+                            "Title": "Couldn't connect to logcat, GetProcessId returned - Xamarin.Android",
+                            "Description": "I started developing mobile apps using Xamarin Forms recently. During the initial days, I had done a lot of research and prepared myself. The development was going good until an issue arised where I had to debug the application in my device,which was not getting deployed in the device…",
+                            "Link": "http://blog.logiticks.com/couldnt-connect-to-logcat-getprocessid-retured-xamarin-android/"
+                        },
+                        {
+                            "Title": "Differentiating Xamarin forms and Xamarin Android",
+                            "Description": "I'm amazed at Xamarin.Forms, a framework for cross-platform mobile app development in C# and runs on any mobile platform. It is very easy to make a mobile application if you know XAML and c#. If you have a .NET background or know how to write C# code and want…",
+                            "Link": "http://blog.logiticks.com/differentiating-xamarin-forms-and-xamarin-android/"
+                        },
+                        {
+                            "Title": "Solution to date & time issue in c3.js",
+                            "Description": "In my previous project, I wanted to show all the result data on a line chart with data in x-axis and status on y-axis. I did google to find an attractive and simple line chart to implement. The problem I had faced was to plot the status against date as…",
+                            "Link": "http://blog.logiticks.com/solution-to-date/"
+                        },
+                        {
+                            "Title": "Relationship between schema and tables in sql",
+                            "Description": "The persistent elements of an SQL are database objects. A database object in a relational database is a data structure used to either store or reference data, schema . A schema is a logical definition of a table - it defines what the name of the table is, and what the…",
+                            "Link": "http://blog.logiticks.com/relationship-between-schema-and-tables-in-sql/"
+                        },
+                        {
+                            "Title": "How to configure Lightweight Directory Access Protocol (LDAP) in Windows",
+                            "Description": " LDAP (Lightweight Directory Access Protocol) is a software protocol for enabling anyone to locate organizations, individuals, and other resources to manage and access the distributed directory information service. LDAP is a lightweight version of Directory Access Protocol (DAP), which is part of X.500. Microsoft includes…",
+                            "Link": "http://blog.logiticks.com/how-to-configure-lightweight-directory-access-protocol-ldap/"
+                        },
+                        {
+                            "Title": "Comparison of Cross-Platform Mobile Development",
+                            "Description": "As the mobile industry grows, mobile technology and the operating system for each mobile have also been tremendously changing. To overcome the concept of developing separate app for each platform(Native coding), there is a different approach called Cross-Platform Development .Lets take a quick look at what exactly…",
+                            "Link": "http://blog.logiticks.com/comparison-of-cross-platform-mobile-development/"
+                        },
+                        {
+                            "Title": "Programmatically check whether a website is down or not",
+                            "Description": "Website and web application are common these days. Each of us have at least one website.For a software development company it may be uncountable. Imagine a scenario when the number of hosted websites is unpredictable and unmanageable and the worst time is when an important site is down…",
+                            "Link": "http://blog.logiticks.com/programically-check-a-website-is-down-or-not/"
+                        },
+                        {
+                            "Title": "Web.config File Transformations",
+                            "Description": "Most of us have worked with web applications using asp.Net. While publishing a web application, we have to change the web configuration according to the server. There are two types of database connection string - local and server side configuration and while…",
+                            "Link": "http://blog.logiticks.com/web-config-file-transformations/"
+                        },
+                        {
+                            "Title": "How to add Certificates in Azure Websites Applications",
+                            "Description": "Introduction Now a day applications are hosted in Azure websites. Some application need certificate for security purposes because those applications may need any API call with in the application. In this blog, I will explain in a step by step procedure to......",
+                            "Link": "http://blog.logiticks.com/how-to-add-certificates-in-azure-websites-applications/"
+                        },
+                        {
+                            "Title": "Export data to excel using MVC",
+                            "Description": "Recently as part of my work I was trying to export data to excel using MVC. Let me admit, I am new to MVC, so my natural inclination was to Google around and if someone has already tried it out. I did manage to get…",
+                            "Link": "http://blog.logiticks.com/export-data-to-excel-in-mvc-at-server-side/"
+                        },
+                        {
+                            "Title": "Creating reusable methods inside view templates in Razor",
+                            "Description": "Hello everyone, Creating reusable methods inside view templates in Razor? Yes, this is a very interesting piece of work that I was struggling to implement and spend a good amount of time to finally get it done.Problem :  I wanted to add a welcome note (good morning, good…",
+                            "Link": "http://blog.logiticks.com/creating-reusable-methods-inside-view-templates-in-razor/"
+                        }
+        ]
